205bb12426399bd9f31d33eecb38c76c3fe3bb19
Ryosuke Ito <rito.0305@gmail.com>
2022-11-07 02:28:11 +0900

    Implement minimal specs

--- spec/danger/danger_plugin_lint_spec.rb
+++ spec/danger/danger_plugin_lint_spec.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
 RSpec.describe Danger::DangerPluginLint do
-  it 'does something useful' do
-    expect(false).to be(true) # rubocop:disable RSpec/ExpectActual
+  include DangerPluginHelper
+  include FixtureHelper
+
+  let(:dangerfile) { testing_dangerfile }
+
+  describe '#lint_docs' do
+    let(:warnings) { dangerfile.status_report[:warnings] }
+    let(:errors) { dangerfile.status_report[:errors] }
+    let(:refs) { [fixture('test_plugin.rb')] }
+
+    context 'when warnings_as_errors is true' do
+      it 'reports everything as error' do
+        dangerfile.plugin_lint.lint_docs(refs, warnings_as_errors: true)
+        expect(warnings).to be_empty
+        expect(errors).to have_attributes size: 6
+      end
+    end
+
+    it 'reports warnings and errors separately' do
+      dangerfile.plugin_lint.lint_docs(refs, warnings_as_errors: false)
+      expect(warnings).to match_array [
+        /reference implementation/,
+        /@tags/,
+        /no useful return value/
+      ]
+      expect(errors).to match_array [
+        /documentation that covers the scope/,
+        /description for your method/,
+        /examples of common use-cases/
+      ]
+    end
   end
 end
