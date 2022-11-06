c18776815623ee477928569bee4c622fbaa1fc06
Ryosuke Ito <rito.0305@gmail.com>
2022-11-06 17:04:24 +0900

    Initial commit
    bundle gem --no-exe --no-coc --no-ext --mit --test=rspec .

---
+++ lib/danger/danger_plugin_lint.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "danger_plugin_lint/version"
+
+module Danger
+  module DangerPluginLint
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
